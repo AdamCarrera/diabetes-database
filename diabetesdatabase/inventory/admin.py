360af6ac1c32055e94b94dd9fac7511f8ff3baaf
AdamCarrera <anc170430@utdallas.edu>
2021-06-17 19:06:05 -0500

    add glucose monitor and care supplies

--- diabetesdatabase/inventory/admin.py
+++ diabetesdatabase/inventory/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
 
-# Register your models here.
+from .models import Insulin, GlucoseMonitor, CareSupplies
+
+@register(Insulin)
+class InsulinAdmin(admin.ModelAdmin):
+    list_display = ['brand', 'quantity', 'type']
+
+@register(GlucoseMonitor)
+class GlucoseMonitorAdmin(admin.ModelAdmin):
+    list_display = ['type', 'quantity']
+
+@register(CareSupplies)
+class CareSuppliesAdmin(admin.ModelAdmin):
+    list_display = ['type', 'quantity']
--- diabetesdatabase/inventory/models.py
+++ diabetesdatabase/inventory/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 
 # Create your models here.
-class Item(models.Model):
+class Insulin(models.Model):
     type = models.CharField(max_length=100)
     quantity = models.IntegerField()
     brand = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+       return self.brand 
+
+class GlucoseMonitor(models.Model):
+    type = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.type
+
+class CareSupplies(models.Model):
+    type = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.type
